899b52180a94105d99c39ddc447bfbf0096c8dd2
home <a25562147@gmail.com>
2023-11-17 02:06:19 +0800

    update half-finished 54-1 (segmentation fault)

--- 9/54-1/svshm_xfr_reader.c
+++ 9/54-1/svshm_xfr_reader.c
@@ -1,61 +1,96 @@
-/*************************************************************************\
-*                  Copyright (C) Michael Kerrisk, 2020.                   *
-*                                                                         *
-* This program is free software. You may use, modify, and redistribute it *
-* under the terms of the GNU General Public License as published by the   *
-* Free Software Foundation, either version 3 or (at your option) any      *
-* later version. This program is distributed without any warranty.  See   *
-* the file COPYING.gpl-v3 for details.                                    *
-\*************************************************************************/
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <semaphore.h>
 
-/* Listing 48-3 */
+#define SEM_NAME "/my_sem"
+#define SHM_NAME "/my_shm"
 
-#include "svshm_xfr.h"
+struct shmseg {                 /* Defines structure of shared memory segment */
+    int cnt;                    /* Number of bytes used in 'buf' */
+    char buf[1024];         /* Data being transferred */
+};
 
-int
-main(int argc, char *argv[])
-{
-    int semid, shmid, xfrs, bytes;
+int main(int argc, char *argv[]) {
+    int xfrs, bytes;
     struct shmseg *shmp;
+    sem_t *sem;
+    int fd;
 
     /* Get IDs for semaphore set and shared memory created by writer */
+    // semid = semget(SEM_KEY, 0, 0);
+    // if (semid == -1)
+    //     errExit("semget");
+    sem = sem_open(SEM_NAME, O_CREAT, 0644, 0); // ignored if semaphore already exists
+    if(sem == SEM_FAILED) {
+        perror("sem_open");
+        return 1;
+    }
 
-    semid = semget(SEM_KEY, 0, 0);
-    if (semid == -1)
-        errExit("semget");
+    // shmid  = shmget(SHM_KEY, 0, 0);
+    // if (shmid == -1)
+    //     errExit("shmget");
+    fd = shm_open(SHM_NAME, O_CREAT | O_RDWR, 0666);
+    if(fd == -1) {
+        perror("shm_open");
+        return 1;
+    }
 
-    shmid  = shmget(SHM_KEY, 0, 0);
-    if (shmid == -1)
-        errExit("shmget");
-
-    shmp = shmat(shmid, NULL, SHM_RDONLY);
-    if (shmp == (void *) -1)
-        errExit("shmat");
+    // shmp = shmat(shmid, NULL, SHM_RDONLY);
+    // if (shmp == (void *) -1)
+    //     errExit("shmat");
+    shmp = mmap(NULL, sizeof(struct shmseg), PROT_READ, MAP_SHARED, fd, 0);
+    if (shmp == MAP_FAILED) {
+        perror("mmap");
+        return 1;
+    }
 
     /* Transfer blocks of data from shared memory to stdout */
 
     for (xfrs = 0, bytes = 0; ; xfrs++) {
-        if (reserveSem(semid, READ_SEM) == -1)          /* Wait for our turn */
-            errExit("reserveSem");
+        // if (reserveSem(semid, READ_SEM) == -1)          /* Wait for our turn */
+        //     errExit("reserveSem");
+        if (sem_wait(sem) == -1) {
+            perror("sem_wait");
+            return 1;
+        }
 
         if (shmp->cnt == 0)                     /* Writer encountered EOF */
             break;
         bytes += shmp->cnt;
 
-        if (write(STDOUT_FILENO, shmp->buf, shmp->cnt) != shmp->cnt)
-            fatal("partial/failed write");
+        if (write(STDOUT_FILENO, shmp->buf, shmp->cnt) != shmp->cnt) {
+            perror("write");
+            return 1;
+        }
 
-        if (releaseSem(semid, WRITE_SEM) == -1)         /* Give writer a turn */
-            errExit("releaseSem");
+        // if (releaseSem(semid, WRITE_SEM) == -1)         /* Give writer a turn */
+        //     errExit("releaseSem");
+        if(sem_post(sem) == -1) {
+            perror("sem_post");
+            return 1;
+        }
     }
 
-    if (shmdt(shmp) == -1)
-        errExit("shmdt");
+    // if (shmdt(shmp) == -1)
+    //     errExit("shmdt");
+    if( munmap(shmp, sizeof(struct shmseg)) == -1) {
+        perror("munmap");
+        return 1;
+    }
 
     /* Give writer one more turn, so it can clean up */
 
-    if (releaseSem(semid, WRITE_SEM) == -1)
-        errExit("releaseSem");
+    // if (releaseSem(semid, WRITE_SEM) == -1)
+    //     errExit("releaseSem");
+    if(sem_post(sem) == -1) {
+        perror("sem_post");
+        return 1;
+    }
 
     fprintf(stderr, "Received %d bytes (%d xfrs)\n", bytes, xfrs);
     exit(EXIT_SUCCESS);
--- 9/54-1/svshm_xfr_writer.c
+++ 9/54-1/svshm_xfr_writer.c
@@ -1,74 +1,157 @@
-/*************************************************************************\
-*                  Copyright (C) Michael Kerrisk, 2020.                   *
-*                                                                         *
-* This program is free software. You may use, modify, and redistribute it *
-* under the terms of the GNU General Public License as published by the   *
-* Free Software Foundation, either version 3 or (at your option) any      *
-* later version. This program is distributed without any warranty.  See   *
-* the file COPYING.gpl-v3 for details.                                    *
-\*************************************************************************/
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <semaphore.h>
 
-/* Listing 48-2 */
+#define SEM_NAME "/my_sem"
+#define SHM_NAME "/my_shm"
 
-#include "semun.h"              /* Definition of semun union */
-#include "svshm_xfr.h"
+struct shmseg {                 /* Defines structure of shared memory segment */
+    int cnt;                    /* Number of bytes used in 'buf' */
+    char buf[1024];         /* Data being transferred */
+};
 
-int
-main(int argc, char *argv[])
-{
-    int semid, shmid, bytes, xfrs;
+int main(int argc, char *argv[]) {
+    int bytes, xfrs;
     struct shmseg *shmp;
-    union semun dummy;
+    // union semun dummy;
+    sem_t *sem;
+    int fd;
 
-    semid = semget(SEM_KEY, 2, IPC_CREAT | OBJ_PERMS);
-    if (semid == -1)
-        errExit("semget");
+    // create semaphore
+    // semid = semget(SEM_KEY, 2, IPC_CREAT | OBJ_PERMS);
+    // if (semid == -1)
+    //     errExit("semget");
 
-    if (initSemAvailable(semid, WRITE_SEM) == -1)
-        errExit("initSemAvailable");
-    if (initSemInUse(semid, READ_SEM) == -1)
-        errExit("initSemInUse");
+    // if (initSemAvailable(semid, WRITE_SEM) == -1)
+    //     errExit("initSemAvailable");
+    // if (initSemInUse(semid, READ_SEM) == -1)
+    //     errExit("initSemInUse");
 
-    shmid = shmget(SHM_KEY, sizeof(struct shmseg), IPC_CREAT | OBJ_PERMS);
-    if (shmid == -1)
-        errExit("shmget");
+    // sem = sem_open(SEM_NAME, O_CREAT, 0644, 1);
+    // if(sem == SEM_FAILED) {
+    //     perror("sem_open");
+    //     return 1;
+    // }
 
-    shmp = shmat(shmid, NULL, 0);
-    if (shmp == (void *) -1)
-        errExit("shmat");
+    // If semaphore exists, delete it and create a new one to reset value.
+    sem = sem_open(SEM_NAME, 0);
+    if (sem != SEM_FAILED) {
+        sem_close(sem);
+        sem_unlink(SEM_NAME);
+    }
+    sem = sem_open(SEM_NAME, O_CREAT, 0644, 1);
+    if(sem == SEM_FAILED) {
+        perror("sem_open");
+        return 1;
+    }
+
+    int value;
+    sem_getvalue(sem, &value);
+    printf("Semaphore value: %d\n", value);
+
+    // create shared memory
+    // shmid = shmget(SHM_KEY, sizeof(struct shmseg), IPC_CREAT | OBJ_PERMS);
+    // if (shmid == -1)
+    //     errExit("shmget");
+    fd = shm_open(SHM_NAME, O_CREAT | O_RDWR, 0666);
+    if(fd == -1) {
+        perror("shm_open");
+        return 1;
+    }
+
+    // shmaat = shared memory attach
+    // shmp = shared memory pointer
+    // shmp = shmat(shmid, NULL, 0);
+    // if (shmp == (void *) -1)
+    //     errExit("shmat");
+
+    // map shared memory object
+    shmp = mmap(NULL, sizeof(struct shmseg), PROT_READ, MAP_SHARED, fd, 0);
+    if (shmp == MAP_FAILED) {
+        perror("mmap");
+        return 1;
+    }
 
     /* Transfer blocks of data from stdin to shared memory */
 
+    // xfrs = transfers (the number of times the transfer happened?)
     for (xfrs = 0, bytes = 0; ; xfrs++, bytes += shmp->cnt) {
-        if (reserveSem(semid, WRITE_SEM) == -1)         /* Wait for our turn */
-            errExit("reserveSem");
+        /* Wait for our turn */
+        // if (reserveSem(semid, WRITE_SEM) == -1)
+        //     errExit("reserveSem");
+        if (sem_wait(sem) == -1) {
+            perror("sem_wait");
+            return 1;
+        }
+        printf("writer pass sem\n");
 
-        shmp->cnt = read(STDIN_FILENO, shmp->buf, BUF_SIZE);
-        if (shmp->cnt == -1)
-            errExit("read");
+        // shmp->cnt = read(STDIN_FILENO, shmp->buf, 1024);
+        // if (shmp->cnt == -1) {
+        //     perror("read");
+        //     return 1;
+        // }
+        if(fgets(shmp->buf, sizeof(shmp->buf), stdin) == NULL) {
+            perror("fgets");
+            return 1;
+        }
+        printf("%s\n", shmp->buf);
+        shmp->cnt = strlen(shmp->buf);
 
-        if (releaseSem(semid, READ_SEM) == -1)          /* Give reader a turn */
-            errExit("releaseSem");
+        // if (releaseSem(semid, READ_SEM) == -1)          /* Give reader a turn */
+        //     errExit("releaseSem");
+        if(sem_post(sem) == -1) {
+            perror("sem_post");
+            return 1;
+        }
 
         /* Have we reached EOF? We test this after giving the reader
            a turn so that it can see the 0 value in shmp->cnt. */
 
-        if (shmp->cnt == 0)
+        if (strcmp(shmp->buf, "quit") == 0)
             break;
     }
 
     /* Wait until reader has let us have one more turn. We then know
        reader has finished, and so we can delete the IPC objects. */
 
-    if (reserveSem(semid, WRITE_SEM) == -1)
-        errExit("reserveSem");
+    // if (reserveSem(semid, WRITE_SEM) == -1)
+    //     errExit("reserveSem");
+    if (sem_wait(sem) == -1) {
+        perror("sem_wait");
+        return 1;
+    }
 
-    if (semctl(semid, 0, IPC_RMID, dummy) == -1)
-        errExit("semctl");
-    if (shmdt(shmp) == -1)
-        errExit("shmdt");
-    if (shmctl(shmid, IPC_RMID, 0) == -1)
-        errExit("shmctl");
+    // if (semctl(semid, 0, IPC_RMID, dummy) == -1)
+    //     errExit("semctl");
+    if(sem_close(sem) == -1) {
+        perror("sem_close");
+        return 1;
+    }
+    if(sem_unlink(SEM_NAME) == -1) {
+        perror("sem_unlink");
+        return 1;
+    }
+    // if (shmdt(shmp) == -1)
+    //     errExit("shmdt");
+    if( munmap(shmp, sizeof(struct shmseg)) == -1) {
+        perror("munmap");
+        return 1;
+    }
+    // if (shmctl(shmid, IPC_RMID, 0) == -1)
+    //     errExit("shmctl");
+    if(close(fd) == -1) {
+        perror("close");
+        return 1;
+    }
+    if(shm_unlink(SHM_NAME) == -1) {
+        perror("shm_unlink");
+        return 1;
+    }
 
     fprintf(stderr, "Sent %d bytes (%d xfrs)\n", bytes, xfrs);
     exit(EXIT_SUCCESS);
